07fd12aeac00492622fce2280c37543dae458134
E7475288 <alvaro_santamaria_cano@carrefour.com>
2023-02-08 13:19:04 +0100

    hello methods

--- vars/log.groovy
+++ vars/log.groovy
@@ -1,7 +1,17 @@
-def info(message) {
-    echo "INFO: ${message}"
-}
+// def info(message) {
+//     echo "INFO: ${message}"
+// }
 
-def warning(message) {
-    echo "WARNING: ${message}"
+// def warning(message) {
+//     echo "WARNING: ${message}"
+// }
+
+def hello(message) {
+    pipeline {
+        stages {
+            stage('Hola mundo') {
+                echo "Hola: ${message}"
+            }
+        }
+    }
 }
