43b1f46d5ef7756cfa10674931e525a463f4fc9b
E7475288 <alvaro_santamaria_cano@carrefour.com>
2023-02-08 13:03:04 +0100

    file structured added

--- src/main.groovy
+++
@@ -1,5 +0,0 @@
-// Checkout git repo
-// Return: Object with git repo information
-def hello() {
-    return 'Hola mundo!'
-}
---
+++ src/org/asantama/main.groovy
@@ -0,0 +1,5 @@
+package org.asantama
+
+def hello() {
+    echo 'Hola mundo!'
+}
